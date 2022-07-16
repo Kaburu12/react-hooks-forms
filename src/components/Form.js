367b2ebdb44ae86997e9597c0aa01539e3f67ce8
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-07-16 18:57:42 +0300

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [ firstName, setFirstName ] = useState("John");
+  const [ lastName, setLastName ] = useState("Henry");
+  const [ newsletter, setNewsLetter ] = useState(false)
+  
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+
+  function handleNewsletterChange(event) { 
+  setNewsLetter(event.target.checked)
+}
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input onChange={handleFirstNameChange} type="text" value={firstName} />
+      <input onChange={handleLastNameChange} type="text" value={lastName} />
+      <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+      <input type="checkbox" id="newsletter" onChange={handleNewsletterChange}
+        /* checked instead of value */
+        checked={newsletter}
+      />
       <button type="submit">Submit</button>
     </form>
   );
---
+++ src/components/ParentComponent.js
@@ -0,0 +1,26 @@
+// import React, { useState } from "react";
+// import Form2 from "./Form2";
+
+// function ParentComponent() {
+//   const [firstName, setFirstName] = useState("John");
+//   const [lastName, setLastName] = useState("Henry");
+
+//   function handleFirstNameChange(event) {
+//     setFirstName(event.target.value);
+//   }
+
+//   function handleLastNameChange(event) {
+//     setLastName(event.target.value);
+//   }
+
+//   return (
+//     <Form2
+//       firstName={firstName}
+//       lastName={lastName}
+//       handleFirstNameChange={handleFirstNameChange}
+//       handleLastNameChange={handleLastNameChange}
+//     />
+//   );
+// }
+
+// export default ParentComponent;
