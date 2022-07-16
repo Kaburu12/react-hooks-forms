7b1f0aa3eca94be213b95204a3e1d8219e68411b
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-07-16 19:00:51 +0300

    commit tree 718a7d77b5626c25e663cb2f16d87169922f2e40

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,34 +1,34 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-function Form() {
-  const [ firstName, setFirstName ] = useState("John");
-  const [ lastName, setLastName ] = useState("Henry");
-  const [ newsletter, setNewsLetter ] = useState(false)
+// function Form() {
+//   const [ firstName, setFirstName ] = useState("John");
+//   const [ lastName, setLastName ] = useState("Henry");
+//   const [ newsletter, setNewsLetter ] = useState(false)
   
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
-  }
+//   function handleFirstNameChange(event) {
+//     setFirstName(event.target.value);
+//   }
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
+//   function handleLastNameChange(event) {
+//     setLastName(event.target.value);
+//   }
 
-  function handleNewsletterChange(event) { 
-  setNewsLetter(event.target.checked)
-}
+//   function handleNewsletterChange(event) { 
+//   setNewsLetter(event.target.checked)
+// }
 
-  return (
-    <form>
-      <input onChange={handleFirstNameChange} type="text" value={firstName} />
-      <input onChange={handleLastNameChange} type="text" value={lastName} />
-      <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
-      <input type="checkbox" id="newsletter" onChange={handleNewsletterChange}
-        /* checked instead of value */
-        checked={newsletter}
-      />
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
+//   return (
+//     <form>
+//       <input onChange={handleFirstNameChange} type="text" value={firstName} />
+//       <input onChange={handleLastNameChange} type="text" value={lastName} />
+//       <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+//       <input type="checkbox" id="newsletter" onChange={handleNewsletterChange}
+//         /* checked instead of value */
+//         checked={newsletter}
+//       />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
 
-export default Form;
+// export default Form;
